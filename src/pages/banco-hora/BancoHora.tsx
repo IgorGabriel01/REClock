825c3f4b7c53899581ff350164f0b2e6068ca1ee
brunno <brunnolinker@gmail.com>
2024-06-04 16:34:20 -0300

    added: -Banco de horas

--- src/pages/banco-hora/BancoHora.tsx
+++ src/pages/banco-hora/BancoHora.tsx
@@ -1,6 +1,10 @@
+import img from '../../assets/img/Group 2.png';
+import img2 from '../../assets/img/Group 2758.png';
+import img3 from '../../assets/img/atual.png';
+import img4 from '../../assets/img/frame.png';
 import { Navigation } from '../../components/navigation-menu/Navigation'
 import styles from './styles.module.scss'
-import HistoryIcon from '@mui/icons-material/History';
+
 
 export const BancoHora: React.FC = () => {
     document.title = 'REClock - Banco de horas'
@@ -11,26 +15,32 @@ export const BancoHora: React.FC = () => {
             <section>
                 <header>
                     <h1>Banco de horas</h1>
-                    <HistoryIcon className={styles.history} />
+                   
                 </header>
                 <div className={styles.container}>
                     <div className={styles.hora}>
                         <h1>Horas semanais</h1>
+                        <img src={img2} alt="" className={styles.img2}/>
                     </div>
 
                     <div className={styles.saldoAtual}>
                         <h1>Saldo atual</h1>
+                        <img src={img3} alt="" className={styles.img3}/>
                     </div>
 
                     <div className={styles.saldoTotal}>
-                        <h1>ei</h1>
+                        <h1>Salto total de horas</h1>
+                        <img 
+                        src={img} 
+                        alt=""/>
                     </div>
                 </div>
 
                 <div 
                 className={styles.bancoHoraSemanal}
                 > 
-                <h1>Banco de horas semanal</h1>
+                <h1>Banco de horas semanais</h1>
+                <img src={img4} alt="" className={styles.img4}/>
                 </div>
             </section>
         </div>
